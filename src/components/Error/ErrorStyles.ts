708aa4b74032f5ad07c5eec95b2187aa974d1ba7
Rouan_Laporal <rouan.laporal@outlook.com>
2023-04-28 14:55:22 +0200

    Style  add style on error component

---
+++ src/components/Error/ErrorStyles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ErrorWrapper = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items:center;
+    margin:30px;
+    background-color: var(--background-light);
+`
+
+export const Illustration = styled.div`
+  max-width: 800px;
+`
+
+export const SubTitle = styled.h2`
+    color: var(--secondary-100);
+`
--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,8 +1,20 @@
+import { ErrorWrapper, Illustration, SubTitle } from "./ErrorStyles";
+import four from "../../assets/404/four.svg"
+import base from "../../assets/404/base.svg"
+import zero from "../../assets/404/zero.svg"
+
 function Error(){
     return (
-        <div>
-            <h1>Oups Cette page n'existe pas</h1>
-        </div>
+        <ErrorWrapper>
+            <h1>Oups...</h1>
+            <Illustration>
+                <img src={four} alt="number_four"/>
+                <img src={zero} alt="number_zero"/>
+                <img src={four} alt="number_four"/>
+                <img src={base} alt="base_illustration"/>
+            </Illustration>
+            <SubTitle>Il semblerait qu'il y ait un problème</SubTitle>
+        </ErrorWrapper>
     )
 }
 
